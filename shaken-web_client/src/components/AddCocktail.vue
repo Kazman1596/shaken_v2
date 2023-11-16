f6265bfdbcc1c32da1ed24867f64a6b106c5b2e6
Stephen Kaczmarowski <srkaz94@gmail.com>
2023-11-16 14:31:29 -0700

    created boilerplate for creating an ingredient within a recipe

--- shaken-web_client/src/components/AddCocktail.vue
+++ shaken-web_client/src/components/AddCocktail.vue
@@ -1,15 +1,42 @@
 <template>
     <div>
         <h2>Create Recipe</h2>
-        <form>
-            <input type="text" id="title" placeholder="Title" />
-            <input type="text" id="ingredients" placeholder="Ingredients" />
-            <input type="text" id="instructions" placeholder="Instructions" />
-            <select id="glass" placeholder="Glassware">
-                <option value="old-fashioned glass">Rocks Glass</option>
+        <input v-model="newRecipe.title" type="text" id="title" placeholder="Title" />
+        <div id="ingredient">
+            <input v-model="newIngredient.quantity" type="number" id="quantity" placeholder="Quantity" />
+            <select v-model="newIngredient.unit" id="unit">
+                <option>N/A</option>
+                <option value="ounce">ounce</option>
+                <option value="drop">drop</option>
+                <option value="pinch">pinch</option>
+                <option value="tablespoon">tablespoon</option>
+                <option value="teaspoon">teaspoon</option>
+                <option value="cup">cup</option>
+                <option value="scoop">scoop</option>
+                <option value="dash">dash</option>
             </select>
-            <button type="submit">Create Recipe</button>
-        </form>
+            <input v-model="newIngredient.name" type="text" id="name" placeholder="Name" />
+            <button v-on="addIngredient()">Add Ingredient</button>
+        </div>
+        <textarea v-model="newRecipe.instructions" type="text" id="instructions" placeholder="Instructions" />
+        <select v-model="newRecipe.glass" id="glass" placeholder="Glassware">
+            <option value="Old-Fashioned Glass">Old-Fashioned Glass</option>
+            <option value="Cocktail Glass">Cocktail Glass</option>
+            <option value="Shot Glass">Shot Glass</option>
+            <option value="Mixing Glass">Mixing Glass</option>
+            <option value="Goblet">Goblet</option>
+            <option value="Beer Mug">Beer Mug</option>
+            <option value="Tankard">Tankard</option>
+            <option value="Collins Glass">Collins Glass</option>
+            <option value="Wine Glass">Wine Glass</option>
+            <option value="Irish Coffee Glass">Irish Coffee Glass</option>
+            <option value="Sour Glass">Sour Glass</option>
+            <option value="Cafe Glass">Cafe Glass</option>
+            <option value="Snifter">Snifter</option>
+            <option value="Highball Glass">Highball Glass</option>
+            <option value="Champagne Flute">Champagne Flute</option>
+        </select>
+        <button v-on:click="createRecipe()">Create Recipe</button>
     </div>
   </template>
   
@@ -19,6 +46,11 @@
     components: {},
     data() {
       return{
+        newIngredient: {
+            quantity: 0,
+            unit: "",
+            name: ""
+        },
         newRecipe: {
             title: "",
             ingredients: [],
@@ -29,7 +61,15 @@
       }
     },
     methods: {
-      
+        createRecipe() {
+            // Create a new recipe
+            // Send to API
+            // Send user home
+        },
+        addIngredient() {
+            // Create a new ingredient
+            // Add it to newRecipe.ingredients array
+        }
     }
   };
   </script>
